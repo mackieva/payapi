7fb553de5756da95032c622d692d13925fb300f8
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-01-24 10:05:51 -0500

    logo grid refactor

--- src/components/home/who-we-work-with.jsx
+++ src/components/home/who-we-work-with.jsx
@@ -1,11 +1,5 @@
 import './who-we-work-with.css';
-
-import tesla from '../../assets/images/shared/desktop/tesla.svg';
-import microsoft from '../../assets/images/shared/desktop/microsoft.svg';
-import hp from '../../assets/images/shared/desktop/hewlett-packard.svg';
-import oracle from '../../assets/images/shared/desktop/oracle.svg';
-import google from '../../assets/images/shared/desktop/google.svg';
-import nvidia from '../../assets/images/shared/desktop/nvidia.svg';
+import LogoGrid from '../shared/logo-grid';
 
 const WhoWeWorkWith = () => {
 	return (
@@ -23,22 +17,7 @@ const WhoWeWorkWith = () => {
 						About Us
 					</a>
 				</div>
-				<div className='wwww__container-logos'>
-					<img src={tesla} alt='Tesla logo' style={{ justifySelf: 'end' }} />
-					<img src={microsoft} alt='Microsoft logo' />
-					<img
-						src={hp}
-						alt='Hewlett Packard logo'
-						style={{ justifySelf: 'start' }}
-					/>
-					<img src={oracle} alt='Oracle logo' style={{ justifySelf: 'end' }} />
-					<img src={google} alt='Google logo' />
-					<img
-						src={nvidia}
-						alt='Nvidia logo'
-						style={{ justifySelf: 'start' }}
-					/>
-				</div>
+				<LogoGrid />
 			</div>
 		</div>
 	);
---
+++ src/components/shared/logo-grid.jsx
@@ -0,0 +1,26 @@
+import './logo-grid.css';
+
+import tesla from '../../assets/images/shared/desktop/tesla.svg';
+import microsoft from '../../assets/images/shared/desktop/microsoft.svg';
+import hp from '../../assets/images/shared/desktop/hewlett-packard.svg';
+import oracle from '../../assets/images/shared/desktop/oracle.svg';
+import google from '../../assets/images/shared/desktop/google.svg';
+import nvidia from '../../assets/images/shared/desktop/nvidia.svg';
+const LogoGrid = () => {
+	return (
+		<div className='logo-grid'>
+			<img src={tesla} alt='Tesla logo' style={{ justifySelf: 'end' }} />
+			<img src={microsoft} alt='Microsoft logo' />
+			<img
+				src={hp}
+				alt='Hewlett Packard logo'
+				style={{ justifySelf: 'start' }}
+			/>
+			<img src={oracle} alt='Oracle logo' style={{ justifySelf: 'end' }} />
+			<img src={google} alt='Google logo' />
+			<img src={nvidia} alt='Nvidia logo' style={{ justifySelf: 'start' }} />
+		</div>
+	);
+};
+
+export default LogoGrid;
