a00da2ab9d8f2a32130762a801e57b74e7a9c8ec
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-01-26 12:45:28 -0500

    installation and routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import './index.css';
+import Home from './Home.jsx';
+import Pricing from './Pricing.jsx';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/pricing' element={<Pricing />} />
+				<Route path='/contact' element={<Contact />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
+);
