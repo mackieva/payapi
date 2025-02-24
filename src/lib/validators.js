99fec7a639bab067c253cea7819a5979c0accc49
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-02-24 15:36:47 -0500

    validator update

--- src/lib/validators.js
+++ src/lib/validators.js
@@ -2,7 +2,10 @@ import { z } from 'zod';
 
 export const contactSchema = z.object({
 	name: z.string().min(1, "This field can't be empty"),
-	email: z.string().email().min(1, "This field can't be empty"),
+	email: z
+		.string()
+		.email('Please use a valid email address')
+		.min(1, "This field can't be empty"),
 	company: z.string().min(1, "This field can't be empty"),
 	title: z.string().min(1, "This field can't be empty"),
 	message: z.string().min(1, "This field can't be empty"),
