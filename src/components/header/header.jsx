1ecd42b22a1cafe35f708f031e4b44d17712f850
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-02-26 08:43:49 -0500

    mobile nav

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,5 @@
 import Nav from '../shared/nav';
+import MobileNav from '../shared/mobile-nav';
 import logo from '../../assets/images/shared/desktop/logo.svg';
 import './header.css';
 import { NavLink } from 'react-router';
@@ -10,9 +11,10 @@ const Header = () => {
 				<img src={logo} alt='logo' className='header__logo' />
 			</NavLink>
 			<Nav />
-			<a href='#demo' className='btn-primary'>
+			<a href='#demo' className='btn-primary header-signup'>
 				Schedule a Demo
 			</a>
+			<MobileNav />
 		</header>
 	);
 };
