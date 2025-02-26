7b00e4061d0f42d7c8b2716b003126c9e06763d1
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-02-26 14:00:45 -0500

    css reset addition

--- src/components/shared/mobile-nav.jsx
+++ src/components/shared/mobile-nav.jsx
@@ -2,6 +2,7 @@ import './mobile-nav.css';
 import { NavLink } from 'react-router';
 import mobileClose from '../../../public/images/close.svg';
 import mobileOpen from '../../../public/images/menu.svg';
+import { createPortal } from 'react-dom';
 import { useState } from 'react';
 
 const MobileNav = () => {
@@ -11,19 +12,22 @@ const MobileNav = () => {
 			<button className='mobile-open' onClick={() => setOpen(true)}>
 				<img src={mobileOpen} alt='Stacked Line Mobile Menu Open Icon' />
 			</button>
-			<div className={`mobile-nav-container ${open ? 'active' : ''}`}>
-				<button onClick={() => setOpen(false)} className='mobile-close'>
-					<img src={mobileClose} alt='X Mobile Menu Close Icon' />
-				</button>
-				<nav className='mobile-nav'>
-					<NavLink to='/pricing'>Pricing</NavLink>
-					<NavLink to='/about'>About</NavLink>
-					<NavLink to='/contact'>Contact</NavLink>
-				</nav>
-				<a href='#demo' className='btn-primary'>
-					Schedule a Demo
-				</a>
-			</div>
+			{createPortal(
+				<div className={`mobile-nav-container ${open ? 'active' : ''}`}>
+					<button onClick={() => setOpen(false)} className='mobile-close'>
+						<img src={mobileClose} alt='X Mobile Menu Close Icon' />
+					</button>
+					<nav className='mobile-nav'>
+						<NavLink to='/pricing'>Pricing</NavLink>
+						<NavLink to='/about'>About</NavLink>
+						<NavLink to='/contact'>Contact</NavLink>
+					</nav>
+					<a href='#demo' className='btn-primary'>
+						Schedule a Demo
+					</a>
+				</div>,
+				document.body
+			)}
 		</>
 	);
 };
